04bde6243b78aea2929f73c435f68dc346819921
Juan Pinto <j.pintos2345@gmail.com>
2023-12-04 16:00:37 -0300

    modificado requirements, setting, urls y models de la app

--- app/models.py
+++ app/models.py
@@ -8,4 +8,5 @@ class Usuario(models.Model):
    Usuario = models.CharField(max_length=10)
    Clave = models.CharField(max_length=50)
    Correo = models.CharField(max_length=150)
- 
+   # BANNER = models.ImageField(upload_to='imagenes/', null=True,verbose_name='algo')
+   # En caso que necesitemos subur una imagen  nuestra base de datos
--- crud_django/urls.py
+++ crud_django/urls.py
@@ -19,7 +19,10 @@ from django.urls import path
 
 from django.urls.conf import include
 
+from django.conf.urls.static import static
+from django.conf import settings   
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api/', include("app.urls")),
-]
+    path('api/', include("app.urls")),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
